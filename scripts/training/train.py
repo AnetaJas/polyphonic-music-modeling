69f0d3b251b9f5046802fc952c964675bd2cb3d8
arybs <arkadiusz.rybski@gmail.com>
2022-05-28 23:33:59 +0200

    Training for LSTM + logger

--- scripts/training/train.py
+++ scripts/training/train.py
@@ -1,6 +1,7 @@
 import logging
 import os
 import warnings
+import datetime 
 
 import hydra
 import omegaconf
@@ -9,6 +10,8 @@ import pytorch_lightning as pl
 import scripts.training.lightning_modules as lm
 import scripts.dataset.dataset_loading as ds
 from polyphonic_music_modeling.model import LSTM as LSTM
+from pytorch_lightning.loggers import WandbLogger
+
 
 from loguru import logger
 
@@ -23,6 +26,7 @@ def main(configs: omegaconf.DictConfig) -> None:
     dataset_module = lm.DataModule(
         batch_size=configs.dataset_module.batch_size,
         num_workers=configs.dataset_module.num_workers,
+        dataset_root=configs.dataset_module.dataset_root
     )
     #dataset_module.setup()
     
@@ -32,13 +36,23 @@ def main(configs: omegaconf.DictConfig) -> None:
         model= LSTM(input_dim=88) # do korelacji ze zbiore danych 
     )
     
+    timestamp = datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+    wandblogger = WandbLogger(
+        project=configs.training.wandb_project,
+        name=f"{configs.training.wandb_name}-{timestamp}",
+        log_model="all" 
+    )
+    
     logger.info("Creating trainer")
     nn_trainer = pl.Trainer(
         max_epochs=configs.training.max_epochs,
+        logger=wandblogger,
+        gpus=1 if configs.training.with_gpu else 0,
     )
 
 
     nn_trainer.fit(nn_module, dataset_module)
+    
     nn_trainer.test(nn_module, dataset_module)
     
 
